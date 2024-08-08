10a885ba7c2946ae50f2d06b938f520118501174
rukhsora31 <ruhsora447@gmail.com>
2024-08-08 20:04:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let car = {
+    brand: 'Maybach',
+    model: 'GLS 600',
+    speed: '250 km/h',
+    released: 2019
+}
+
+car.color = 'brown'
+car.released = 2024
+
+console.log(car);
+
+let person = {
+    name: 'Gavi',
+    surname: 'Paes Gavira',
+    age: 20
+}
+
+let person2 = {...person}
+console.log(person2);
+
